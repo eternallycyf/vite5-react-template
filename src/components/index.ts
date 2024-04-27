995723735690b88db108b23c8b5da73a0ed393e7
eternallycyf <969475322@qq.com>
2024-04-27 16:07:33 +0800

    ⏪ revert: spell error

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './Loading'
+export * from './Icon'
+export * from './Animate'
+export { default as ProgressBar } from './ProgressBar'
+export { default as ScrollBar } from './ScrollBar'
